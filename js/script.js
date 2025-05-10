0b0981561024ef0deac652d88d1e3d0d7b95d4a0
vale46tetsu <vale46tetsu@gmail.com>
2025-05-10 23:51:55 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-// ===== ハンバーガーメニュー制御 =====
-const burger   = document.getElementById('burger');
-const navMenu  = document.getElementById('nav-menu');
+// ハンバーガーメニュー制御
+const burger = document.getElementById('burger');
+const navMenu = document.getElementById('nav-menu');
 
 burger.addEventListener('click', () => {
   navMenu.classList.toggle('open');
