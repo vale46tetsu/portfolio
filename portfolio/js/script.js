d41067bee037d53ed9d6ae2cdbf75dd59b2c15cd
vale46tetsu <vale46tetsu@gmail.com>
2025-05-10 23:18:50 +0900

    Delete portfolio directory

--- portfolio/js/script.js
+++
@@ -1,7 +0,0 @@
-// ===== ハンバーガーメニュー制御 =====
-const burger   = document.getElementById('burger');
-const navMenu  = document.getElementById('nav-menu');
-
-burger.addEventListener('click', () => {
-  navMenu.classList.toggle('open');
-});
